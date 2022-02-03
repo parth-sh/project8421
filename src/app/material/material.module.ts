34c2d15d6b1777740e211ac38e6267b136dce92f
parth <parth@tradecred.com>
2022-02-03 19:07:22 +0530

    drawer configuration

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 
-import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 const MaterialComponents = [
-  MatButtonModule
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatSidenavModule
 ]
 
 @NgModule({
