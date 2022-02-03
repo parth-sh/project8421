9ac86c03c040593cb8a6db948a79dbd5a3212f63
parth <parth@tradecred.com>
2022-02-04 01:37:35 +0530

    unnnecesarry update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '', component: DashboardComponent },
 ];
 
